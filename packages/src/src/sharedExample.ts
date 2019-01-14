5b41dbe3897e69c558ad948d5233fa8c4ec55e27
Charlie Jackson <charlie@charliejackson.com>
2019-01-14 11:13:43 +0000

    ✨ feat(src): Share src dir with web
    - Original Branch: master

---
+++ packages/src/src/sharedExample.ts
@@ -0,0 +1,7 @@
+/**
+ * Adds 2 numbers together
+ */
+const sharedExample = (param1: number, param2: number): number =>
+  param1 + param2;
+
+export default sharedExample;
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import sharedExample from "@cajacko/src/lib/sharedExample";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+// tslint:disable-next-line
+console.log(sharedExample(1, 2));
+
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
