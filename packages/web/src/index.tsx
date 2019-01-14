a5d88806e7e347e23da6568efefefd73f21a9b28
Charlie Jackson <charlie@charliejackson.com>
2019-01-14 14:02:34 +0000

    ✨ feat(web): Eject web
    - Original Branch: master

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,12 +1,8 @@
-import sharedExample from "@cajacko/src/lib/sharedExample";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
-
-// tslint:disable-next-line
-console.log(sharedExample(1, 2));
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
