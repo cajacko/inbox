affbff87cb8cf2fb0d536560c01a0accd2ef67d3
Charlie Jackson <charlie@charliejackson.com>
2019-01-21 16:19:05 +0000

    🛠 refactor(typescript): Fix the typescript config for functions
    - Original Branch: master

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as functions from 'firebase-functions';
-import sharedExample from './lib/sharedExample';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send(`Hello my friends!${sharedExample(1, 2)}`);
+  response.send('Hello my friends!');
 });
